f633caf2b72822d955da5ecf0561aaef1f2c7618
rohit george <code.mastermind.07@gmail.com>
2023-04-28 23:09:07 +0530

    updated login page

---
+++ src/components/DisplayResult.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const DisplayResult = () => {
+  return (
+    <div>
+        <Navbar/>
+        
+    </div>
+  )
+}
+
+export default DisplayResult
