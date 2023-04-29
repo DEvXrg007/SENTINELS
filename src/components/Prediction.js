68e2a5c87e55eb691f4d439265683739f3ed34c7
rohit george <code.mastermind.07@gmail.com>
2023-04-29 08:50:11 +0530

    final commit

--- src/components/Prediction.js
+++ src/components/Prediction.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Prediction = () => {
   return (
-    <div className='h-screen w-screen flex items-center justify-center'>
-        Eventually you are gonna die
+    <div className='h-screen w-screen flex items-center justify-center text-2xl'>
+       you may have the following in future
+      <ul className=' list-none'>
+        <li className='mx-4'>stroke</li>
+        <li className='mx-4'>coronary artery disease</li>
+        <li className='mx-4'>memory loss</li>
+        <li className='mx-4'>weakness</li>
+      </ul>
     </div>
   )
 }
